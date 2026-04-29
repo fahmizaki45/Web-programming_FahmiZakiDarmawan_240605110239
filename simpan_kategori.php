8ff23e0b9d9ea7e11f8b6c7240ad8c18d59af8c9
fahmizaki45 <fhmzkdrmwn45@gmail.com>
2026-04-29 09:14:54 +0700

    Delete simpan_kategori.php

--- simpan_kategori.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'koneksi.php';
-$nama = $_POST['nama_kategori'];
-$ket = $_POST['keterangan'];
-$stmt = $conn->prepare("INSERT INTO kategori_artikel (nama_kategori, keterangan) VALUES (?, ?)");
-$stmt->bind_param("ss", $nama, $ket);
-$stmt->execute() ? print("Kategori disimpan") : print("Gagal: Nama kategori sudah ada");
-?>
