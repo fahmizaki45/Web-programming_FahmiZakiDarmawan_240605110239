1945c0069d51487211febe8b36962f7a15d972e7
fahmizaki45 <fhmzkdrmwn45@gmail.com>
2026-04-29 09:16:16 +0700

    Delete ambil_satu_penulis.php

--- ambil_satu_penulis.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'koneksi.php';
-$id = $_GET['id'];
-$stmt = $conn->prepare("SELECT id, nama_depan, nama_belakang, user_name, foto FROM penulis WHERE id = ?");
-$stmt->bind_param("i", $id);
-$stmt->execute();
-echo json_encode($stmt->get_result()->fetch_assoc());
-?>
